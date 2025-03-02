9720dd1e6c411482e1c38b1258f7324b1d8188d2
zanamira43 <zanamira43@gmail.com>
2025-03-02 09:54:19 +0300

    configured CORS middleware inside  main.go

--- main.go
+++ main.go
@@ -35,8 +35,9 @@ func main() {
 
 	// deal with cors for frontend
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000", "http://*"},
-		// AllowMethods:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://*", "https://*"},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods:     []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
 		AllowCredentials: true,
 	}))
 
