41a16ba275e8b9f1cf57327040cbcf6e2b497afe
zanamira43 <zanamira43@gmail.com>
2025-09-27 22:08:03 +0300

    set seding email time to 10:10 pm

--- utils/time_table_send_email.go
+++ utils/time_table_send_email.go
@@ -48,7 +48,7 @@ func StartDailyEmailJob() {
 	c := cron.New(cron.WithLocation(loc))
 
 	// Runs every day at 4 AM
-	c.AddFunc("35 21 * * *", func() {
+	c.AddFunc("10 22 * * *", func() {
 		log.Printf("Test cron job running at: %v", time.Now())
 		today := time.Now().In(loc).Weekday().String() // Get today's day
 		NotifyUsersByDay(today)
