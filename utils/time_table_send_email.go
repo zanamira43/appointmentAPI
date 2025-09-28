9cb313e31dbf2f3ae5fc98c3147a82c59c69f333
zanamira43 <zanamira43@gmail.com>
2025-09-28 10:05:30 +0300

    change email smtp port with email time sending

--- utils/time_table_send_email.go
+++ utils/time_table_send_email.go
@@ -27,7 +27,7 @@ func SendEmail(to, subject, body string) error {
 	password := os.Getenv("SMTP_PASSWORD")
 
 	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	smtpPort := 465
 
 	msg := []byte("To: " + to + "\r\n" +
 		"Subject: " + subject + "\r\n" +
@@ -48,7 +48,7 @@ func StartDailyEmailJob() {
 	c := cron.New(cron.WithLocation(loc))
 
 	// Runs every day at 4 AM
-	c.AddFunc("55 9 * * *", func() {
+	c.AddFunc("10 10 * * *", func() {
 		log.Printf("Test cron job running at: %v", time.Now())
 		today := time.Now().In(loc).Weekday().String() // Get today's day
 		NotifyUsersByDay(today)
